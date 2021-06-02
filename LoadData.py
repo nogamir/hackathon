6f84a444918e6c4f83b863c3f75099af9417ead6
YuvalGrader <yuvalgrader@cs.huji.ac.il>
2021-06-03 00:42:22 +0300

    T3

--- LoadData.py
+++ LoadData.py
@@ -3,8 +3,10 @@ import sys
 sys.path.append("../")
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
+
+# consts
+NA = "Na"
+
 
 def load_data(path_to_file):
     """
@@ -16,9 +18,18 @@ def load_data(path_to_file):
     df = pd.read_csv(path_to_file)
     # df.insert(0, 0, 1)  # adding intersect
     df = df.loc[df["revenue"] > 0]
+    df["belongs_to_collection_name"] = df.apply(lambda row: get_feature_from_text_dict(row.belongs_to_collection, "name"), axis=1)
     return df
+
+
+def get_feature_from_text_dict(s, field):
+    if type(s) == str:
+        d = eval(s)
+        return d[field]
+    return NA
+
 
 if __name__ == '__main__':
     path = "movies_dataset.csv"
     X = load_data(path)
-    print(X.shape)
+    print(X.head())
