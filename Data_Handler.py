ec112d43da5d425af286b88626c90b43e8c1fe64
gmatan318 <matan.gilboa@cs.huji.ac.il>
2021-06-03 14:44:13 +0300

    remove rare columns

--- Data_Handler.py
+++ Data_Handler.py
@@ -32,18 +32,16 @@ def get_month(str):
 def sum_column(col):
     pass
 
-def remove_rare_columns(df, original_feature_name, new_features, percentile):
+def remove_rare_columns(new_features, percentile):
     #  find threshold:
     feature_frequency_col = new_features[:, 2]
     threshold = np.percentile(feature_frequency_col, percentile, axis=0)
-
-    #  find common columns:
+    #  return common features list:
     common_features = new_features[feature_frequency_col >= threshold]
-    for feature in common_features:
-        feature_name = f'{original_feature_name}_{feature[0]}'
-        movies_indicator_vec = np.zeros(len(df))
-        movies_indicator_vec.put(feature[1], 1)
-        df[feature_name] = movies_indicator_vec
+    sum = common_features[:, 2].sum()
+    common_features[:, 2] = common_features[:, 2] / sum
+    return common_features[:, [0, 2]]
+
 
 
 
@@ -53,5 +51,5 @@ if __name__ == '__main__':
     new_features = np.array([["bradd", (1, 3, 5), 3],
                              ["angelina", (2, 3, 6, 8), 4],
                              ["jake", (4,), 1]])
-    remove_rare_columns(X, "cast", new_features, 0.8)
+    remove_rare_columns(new_features, 50)
 
